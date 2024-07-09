c1f6c3508508690bcdc5b112b4286de816440b5b
ANKropotov <ANKropotov@inno.tech>
2024-07-09 20:08:13 +0500

    + CorporateSettlementAccountController

--- accounts-app/src/main/java/dev/kropotov/accounts/controller/CorporateSettlementAccountController.java
+++ accounts-app/src/main/java/dev/kropotov/accounts/controller/CorporateSettlementAccountController.java
@@ -4,8 +4,12 @@ import dev.kropotov.accounts.controller.api.CorporateSettlementAccountApi;
 import dev.kropotov.accounts.dto.common.ResponsePayload;
 import dev.kropotov.accounts.dto.CorporateSettlementAccountRequestDto;
 import dev.kropotov.accounts.dto.CorporateSettlementAccountResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequiredArgsConstructor
 public class CorporateSettlementAccountController implements CorporateSettlementAccountApi {
     @Override
     public ResponseEntity<ResponsePayload<CorporateSettlementAccountResponseDto>> create(
