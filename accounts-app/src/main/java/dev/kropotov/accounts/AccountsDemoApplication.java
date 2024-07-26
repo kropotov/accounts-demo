a01b46e4daeb07ac0954b75559b971455afa9db2
ANKropotov <ANKropotov@inno.tech>
2024-07-26 18:53:59 +0500

    убрал @EnableJpaRepositories

--- accounts-app/src/main/java/dev/kropotov/accounts/AccountsDemoApplication.java
+++ accounts-app/src/main/java/dev/kropotov/accounts/AccountsDemoApplication.java
@@ -2,10 +2,12 @@ package dev.kropotov.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
-@ComponentScan("dev.kropotov.accounts.*")
+@SpringBootApplication(scanBasePackages = "dev.kropotov.accounts")
+@EntityScan("dev.kropotov.accounts.entity")
+@EnableTransactionManagement
 public class AccountsDemoApplication {
 
     public static void main(String[] args) {
